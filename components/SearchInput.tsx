d1ca3950bcd0c96ff182bb9fc90882ab395e3599
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-15 16:26:23 +0300

    song search page

---
+++ components/SearchInput.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+
+import useDebounce from "@/hooks/useDebounce";
+
+import Input from "./Input";
+
+interface SearchInputProps {}
+
+const SearchInput: React.FC<SearchInputProps> = ({}) => {
+	const router = useRouter();
+	const [value, setValue] = useState<string>("");
+	const debounceValue = useDebounce<string>(value, 500);
+
+	useEffect(() => {
+		const query = {
+			title: debounceValue,
+		};
+
+		const url = qs.stringifyUrl({
+			url: "/search",
+			query: query,
+		});
+
+		router.push(url);
+	}, [debounceValue, router]);
+
+	return (
+		<Input
+			placeholder="What do you want to listen to?"
+			value={value}
+			onChange={(e) => setValue(e.target.value)}
+		/>
+	);
+};
+
+export default SearchInput;
