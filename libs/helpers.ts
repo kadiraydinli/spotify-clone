512e030ed7c3629dfa1ef676a19124dea1cd7002
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-18 15:51:44 +0300

    stripe integration

---
+++ libs/helpers.ts
@@ -0,0 +1,45 @@
+import { Price } from "@/types";
+
+export const getURL = () => {
+    let url =
+        process.env.NEXT_PUBLIC_SITE_URL ??
+        process.env.NEXT_PUBLIC_VERCEL_URL ??
+        'http://localhost:3000/';
+
+    url = url.includes('http') ? url : `http://${url}`;
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+
+    return url;
+};
+
+export const postData = async ({
+    url,
+    data
+}: {
+    url: string,
+    data?: { price: Price }
+}) => {
+    console.log('POST REQUEST: ', url, data);
+
+    const res: Response = await fetch(url, {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        credentials: 'same-origin',
+        body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+        console.log("Error in POST", { url, data, res });
+
+        throw new Error(res.statusText);
+    }
+
+    return res.json();
+};
+
+export const toDateTime = (sec: number) => {
+    var t = new Date('1970-01-01T00:30:00Z');
+    t.setSeconds(sec);
+
+    return t;
+};
